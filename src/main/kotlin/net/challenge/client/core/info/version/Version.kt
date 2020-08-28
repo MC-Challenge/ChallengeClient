b59d64bc74770315b913d54c7b7956d078582728
FB_01 <58854363+BergerAPI@users.noreply.github.com>
2020-08-28 18:56:23 +0200

    Updated Version
    Added Documentations to 'toInteger'

--- src/main/kotlin/net/challenge/client/core/info/version/Version.kt
+++ src/main/kotlin/net/challenge/client/core/info/version/Version.kt
@@ -2,6 +2,10 @@ package net.challenge.client.core.info.version
 
 class Version(private val major: Int, private val minor: Int, private val patch: Int) : IVersion {
 
+    /**
+     * @return The version formatted as `$major.$minor.$patch` (Integer)
+     */
+    
     override fun toInteger(): Int {
         return "$major.$minor.$patch".toInt()
     }
