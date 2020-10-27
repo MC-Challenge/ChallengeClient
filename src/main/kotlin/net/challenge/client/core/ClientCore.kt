84e363beef3bd3fdc1b9fa158f7686963e833e95
Madakai <madakai.code@gmail.com>
2020-10-27 08:04:09 +0100

    Same-Height Challenge

--- src/main/kotlin/net/challenge/client/core/ClientCore.kt
+++ src/main/kotlin/net/challenge/client/core/ClientCore.kt
@@ -30,6 +30,7 @@ import net.challenge.client.features.commands.impl.CmdToggleCosmetic
 import net.challenge.client.features.cosmetics.registry.CosmeticRegistry
 import net.challenge.client.features.cosmetics.registry.ICosmeticRegistry
 import net.challenge.client.features.modules.impl.challenge.ReachChallenge
+import net.challenge.client.features.modules.impl.challenge.SameHeightChallenge
 import net.challenge.client.features.modules.impl.hud.*
 import net.challenge.client.features.modules.registry.IModuleRegistry
 import net.challenge.client.features.modules.registry.ModuleRegistry
@@ -98,7 +99,8 @@ object ClientCore : IClientCore {
                 HudXYZ,
                 ReachChallenge,
                 HudReach,
-                HudTime
+                HudTime,
+                SameHeightChallenge(TeammateHandler)
         )
 
         moduleRegistry.load()
